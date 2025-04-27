6d981073d3f04ee2ad009a96638d705e035351d8
Diego Ramirez <diego0587747@gmail.com>
2025-04-26 19:04:48 -0600

    changed the get_students_in_course function to include professors and TAs

--- get_students_in_course.py
+++ get_students_in_course.py
@@ -1,43 +1,32 @@
 import requests
 from dotenv import load_dotenv
 import os
+from canvasapi import Canvas
 
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
-COURSE_ID = "COURSE_ID"
-BASE_URL = 'https://canvas.du.edu/api/v1/'
+COURSE_ID = "190476"
+BASE_URL = 'https://canvas.du.edu'
 
-headers = {
-        'Authorization': f'Bearer {API_KEY}'
-    }
-
-def get_students_in_course(COURSE_ID: str)->list:
+def get_people_in_course(COURSE_ID: str)->list:
     """
     Takes the course ID as a string,
     returns a list of the students in the course.
     """
-    
-    url = f'{BASE_URL}courses/{COURSE_ID}/students'
+    canvas = Canvas(BASE_URL, API_KEY)
 
-    response = requests.get(url, headers=headers)
+    course = canvas.get_course(COURSE_ID)
+    students = course.get_users(enrollment_type=["student"])
+    tas = course.get_users(enrollment_type=["ta"])
+    professors = course.get_users(enrollment_type=["teacher"])
 
-    if response.status_code == 200:
-        # Convert JSON response into a Python dictionary
-        data = response.json()
-        
-        students =[] #instantiate list of students
 
-        # Access the 'name' field
-        for item in data:
-            name = item['name']
-            # print(f'{name}')
-            students.append(name)
-        
-        # print(f"These are the students in your course: {students}")
-    else:
-        return ImportError(f"Failed to retrieve data. Status code: {response.status_code}") #idk if right error
+    for student in students:
+        print(f"{student}: Student")
+    for ta in tas:
+        print(f"{ta}: TA")
+    for professor in professors:
+        print(f"{professor}: Professor")
 
-    return students
-
-print(get_students_in_course(COURSE_ID))
+get_people_in_course(COURSE_ID)
